74d4f5cfa24dd7a1072d3385bf0af6840dd0109f
Sumant Tiwary <tech.sumanttiwary@gmail.com>
2025-08-13 01:32:59 +0530

    Update UserServiceImpl.java

--- src/main/java/com/swasthyamitra/healthportal/service/impl/UserServiceImpl.java
+++ src/main/java/com/swasthyamitra/healthportal/service/impl/UserServiceImpl.java
@@ -73,6 +73,7 @@ public class UserServiceImpl implements UserService {
         }
 
         UserInfoEntity userInfoEntity = mapper.convertUserRequestToUserInfoEntity(userRequestVO);
+        userInfoEntity.setDeleted(userRequestVO.getRole().equalsIgnoreCase("USER"));
         userInfoEntity.setEmail(userRequestVO.getEmail());
         userInfoEntity.setCreatedBy(userRequestVO.getCreatedBy());
         userInfoEntity.setPassword(userRequestVO.getPassword());
